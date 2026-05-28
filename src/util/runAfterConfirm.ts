82b3bb5adb877983cc7b48346c23f6c394c5ecbd
InkerBot <im@inker.bot>
2026-05-28 18:30:59 +0800

    remove unused file

--- src/util/runAfterConfirm.ts
+++
@@ -1,8 +0,0 @@
-export default async function runAfterConfirm<T>(message: string, action: () => T): Promise<T> {
-  const confirmed = window.confirm(message);
-  if (confirmed) {
-    return action();
-  } else {
-    throw new Error("Action was not confirmed");
-  }
-}
